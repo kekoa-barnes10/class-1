f7b6ec130e2c1145800ee2f12fe81ecfe68894e4
kekoa-barnes10 <kekoabarnes10@gmail.com>
2024-09-27 21:15:27 -0700

    Add files via upload

---
+++ quiz 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Car {
+private:
+    string model;
+    int year;          
+
+public:
+    
+    Car(const string& carModel, int carYear)  model(carModel), year(carYear) 
+    {
+       cout << "Car created: " << model << ", Year: " << year << endl;
+    }
+    Car() 
+    {
+       cout << "Car destroyed: " << model << endl;
+    }
+  
+};
+int main() 
+{
+    string car = "BMW M4 ";
+    int year = 2020;
+    Car myCar(car, year);
+ cout << "Model: " << car << " Year: " << year << endl;
+    return 0;
+}
+
+
+// quiz 2 kekoa barnes 
